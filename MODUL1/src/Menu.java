a9485b02bcdaca731dbaf582d6b4eb3fb453aaaa
mutialifina <mutialifina@student.telkomuniversity.ac.id>
2022-11-02 16:33:50 +0700

    MODUL 1

---
+++ MODUL1/src/Menu.java
@@ -0,0 +1,19 @@
+public class Menu {
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    String name;
+    String category;
+    int price;
+
+    public void getName(String name){
+        this.name = name;
+    }
+
+    public void getCategory(String category){
+        this.category = category;
+    }
+    
+    public void getPrice(String price){
+        this.price = price;
+    }
+    
+}
---
+++ MODUL1/src/Server.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO Create Database
+
+        // TODO Create Menu
+        for (int i = 0; i < 5; i++) {
+            new Menu();
+            System.out.println("1. Pilih Menu");
+        }
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu and User must be Register First
+
+        // TODO Create User from register in Main Menu
+
+        // TODO Display Menu
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Register terlebih dahulu");
+        System.out.println("==================================");
+        System.out.println("Terima kasih");
+
+
+    }
+}
